899eed9826a4a31f714737a7351b95abfa6c9f98
M. Mobasher Reza <30533488+nishatreza@users.noreply.github.com>
2022-01-15 01:04:22 +0600

    4th
    added listview

--- base/views.py
+++ base/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
+# from django.shortcuts import render
+# from django.http import HttpResponse
+from django.views.generic.list import ListView
+from .models import Task
 
 
-def taskList(request):
-    return HttpResponse('to do list')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
